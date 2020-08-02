f3d3f3641fe40b00a1654f2b8ad539b55f7731db
YuHChen <yuhchen21@gmail.com>
2020-08-01 20:49:37 -0400

    Add barebones atkit homepage.

---
+++ src/Atkit.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Atkit() {
+    return (
+        <div className="atkit">
+            <header>
+                Welcome to atkit!
+            </header>
+        </div>
+    );
+}
+
+export default Atkit;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Atkit from './Atkit';
+
+import './index.css';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Atkit />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
