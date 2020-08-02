0567104cb88a1605bbb3e06a4bdbe00f91c14275
YuHChen <yuhchen21@gmail.com>
2020-08-01 23:34:53 -0400

    Add support for linting and formatting.

--- src/Atkit.tsx
+++ src/Atkit.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
+import React from "react";
 
 function Atkit() {
-    return (
-        <div className="atkit">
-            <header>
-                Welcome to atkit!
-            </header>
-        </div>
-    );
+  return (
+    <div className="atkit">
+      <header>Welcome to atkit!</header>
+    </div>
+  );
 }
 
 export default Atkit;
