994f27cb94d311281fd1c97b45e57459895152b7
YuHChen <yuhchen21@gmail.com>
2020-08-08 18:46:08 -0400

    Updates

---
+++ precache-manifest.9a21bfd8ad87a729dcc8a4f16b260b23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e02c65c2363970604c166e69b529628",
+    "url": "/atkit/index.html"
+  },
+  {
+    "revision": "ef6b01177b166fb5c882",
+    "url": "/atkit/static/css/main.ddc889cf.chunk.css"
+  },
+  {
+    "revision": "c692a9a2be67e910b69d",
+    "url": "/atkit/static/js/2.9fbde058.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/atkit/static/js/2.9fbde058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef6b01177b166fb5c882",
+    "url": "/atkit/static/js/main.17de319a.chunk.js"
+  },
+  {
+    "revision": "19073e6e8e8a7ce67b32",
+    "url": "/atkit/static/js/runtime-main.01b0a249.js"
+  },
+  {
+    "revision": "2571aa7d06dfa050646dad40fb67106a",
+    "url": "/atkit/static/media/logo.2571aa7d.png"
+  }
+]);
