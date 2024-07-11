fefb0a19643ab9aa5b36d0ee42da6bc2e8c5b4ce
YuHChen <yuhchen21@gmail.com>
2024-07-11 23:14:56 +0900

    Add cards data dumper.

--- src/devtools/dataDumpers.tsx
+++ src/devtools/dataDumpers.tsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
+
+import { CachedAnimationThrowdownApi } from "../data/api";
+import type { AnimationThrowdownApi, ApiResult } from "../data/api/types";
 import Wip from "../Wip";
 
-const CardsDataDumper: React.FC = () => (
-  <Wip pageName="cards data dumper page" />
-);
+import DataDumper from "./DataDumper";
+
+const useCardsData = (
+  api: AnimationThrowdownApi = CachedAnimationThrowdownApi
+) => {
+  const [cardsData, setCardsData] = useState<ApiResult>(undefined);
+
+  useEffect(() => {
+    if (!cardsData) {
+      api.fetchCardsData().then(setCardsData).catch(console.log);
+    }
+  }, [api, cardsData]);
+
+  return cardsData;
+};
+
+const CardsDataDumper: React.FC = () => <DataDumper data={useCardsData()} />;
 
 const CombosDataDumper: React.FC = () => (
   <Wip pageName="combos data dumper page" />
